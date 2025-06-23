d0b70ac0b70e994fee4de0a008e95c3a72df9542
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-23 11:01:39 -0300

    Adiciona componente Cabecalho

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
+import Cabecalho from "./Cabecalho";
+
 export default function App() {
-  // Comentário de uma linha
-  /* Comentário de múltiplas linhas */
   return (
     <>
-      <header>
-        <h1>Olá React! 😎</h1>
-        <hr />
-        <nav>
-          <a href="">Home</a>
-          <a href="">Sobre</a>
-          <a href="">Contato</a>
-        </nav>
-      </header>
-
-      {/* Comentários no meio do JSX: é normal ficar entre {} */}
-      {/* Ao usar outros elementos HTML no JSX, é importante
-      envolver tudo em um único <> </> (Fragmento React) */}
+      <Cabecalho />
 
       <main>
         <section>
---
+++ src/Cabecalho.tsx
@@ -0,0 +1,13 @@
+export default function Cabecalho() {
+  return (
+    <header>
+      <h1>Olá React! 😎</h1>
+      <hr />
+      <nav>
+        <a href="">Home</a>
+        <a href="">Sobre</a>
+        <a href="">Contato</a>
+      </nav>
+    </header>
+  );
+}
