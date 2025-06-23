eb2f293db8771db88b9415a40a9b03f7e7c60b78
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-23 11:40:53 -0300

    Corrige exercício componentes
    Conteudo e Rodape

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,15 @@
 import Cabecalho from "./Cabecalho";
+import Conteudo from "./Conteudo";
+import Rodape from "./Rodape";
 
 export default function App() {
   return (
     <>
       <Cabecalho />
 
-      <main>
-        <section>
-          <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
-          <p>Este é um exemplo de aplicação React.</p>
+      <Conteudo />
 
-          <article>
-            <h3>Artigo 1 de exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Asperiores, et voluptatem dignissimos dolorum necessitatibus sint
-              quod consectetur aliquid! Officiis adipisci totam natus dicta
-              laborum quisquam fugit corrupti nesciunt, quasi sed?
-            </p>
-          </article>
-
-          <article>
-            <h3>Artigo 2 de exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Asperiores, et voluptatem dignissimos dolorum necessitatibus sint
-              quod consectetur aliquid! Officiis adipisci totam natus dicta
-              laborum quisquam fugit corrupti nesciunt, quasi sed?
-            </p>
-          </article>
-
-          <article>
-            <h3>Artigo 3 de exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Asperiores, et voluptatem dignissimos dolorum necessitatibus sint
-              quod consectetur aliquid! Officiis adipisci totam natus dicta
-              laborum quisquam fugit corrupti nesciunt, quasi sed?
-            </p>
-          </article>
-        </section>
-      </main>
-
-      <footer>
-        <h2>Pequeno site criado com React</h2>
-        <p>
-          Desenvolvido por <b>Tiago</b> &copy; 2025
-        </p>
-      </footer>
+      <Rodape />
     </>
   );
 }
---
+++ src/Rodape.tsx
@@ -0,0 +1,10 @@
+export default function Rodape() {
+  return (
+    <footer>
+      <h2>Pequeno site criado com React</h2>
+      <p>
+        Desenvolvido por <b>Tiago</b> &copy; 2025
+      </p>
+    </footer>
+  );
+}
