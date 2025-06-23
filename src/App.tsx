55e83c25754a344a72e9378696535329978852eb
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-23 10:02:53 -0300

    Usa fragmento ao redor dos elementos

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ export default function App() {
   // Comentário de uma linha
   /* Comentário de múltiplas linhas */
   return (
-    <header>
-      <h1>Olá React! 😎</h1>
-      <hr />
-      <nav>
-        <a href="">Home</a>
-        <a href="">Sobre</a>
-        <a href="">Contato</a>
-      </nav>
-    </header>
+    <>
+      <header>
+        <h1>Olá React! 😎</h1>
+        <hr />
+        <nav>
+          <a href="">Home</a>
+          <a href="">Sobre</a>
+          <a href="">Contato</a>
+        </nav>
+      </header>
+      <main></main>
+    </>
   );
 }
