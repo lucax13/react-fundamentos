0b0b1584f9d9d32d46d5be930de282c153c2b7cb
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-23 11:14:34 -0300

    Importa imagem para o componente

--- src/Cabecalho.tsx
+++ src/Cabecalho.tsx
@@ -1,7 +1,13 @@
+/* Tudo o que for usado dentro de componentes (imagens, fontes etc), 
+deve ser importado. Desta forma, no momento do build, haverá otimizações para este conteúdo. */
+import logo from "./assets/logo-react.png";
+
 export default function Cabecalho() {
   return (
     <header>
-      <h1>Olá React! 😎</h1>
+      <h1>
+        <img src={logo} alt="" /> Olá React! 😎
+      </h1>
       <hr />
       <nav>
         <a href="">Home</a>
