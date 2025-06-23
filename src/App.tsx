eddb5f5c24e89402cd458473e8ca6741647b512f
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-23 09:55:32 -0300

    Adiciona menu

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ export default function App() {
     <header>
       <h1>Olá React! 😎</h1>
       <hr />
+      <nav>
+        <a href="">Home</a>
+        <a href="">Sobre</a>
+        <a href="">Contato</a>
+      </nav>
     </header>
   );
 }
