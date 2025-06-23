fa0b67231a007e52e5c7dfaf2a26906b83473167
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-23 10:36:10 -0300

    Adiciona outros elementos ao componente

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,54 @@ export default function App() {
           <a href="">Contato</a>
         </nav>
       </header>
-      <main></main>
+
+      {/* Comentários no meio do JSX: é normal ficar entre {} */}
+      {/* Ao usar outros elementos HTML no JSX, é importante
+      envolver tudo em um único <> </> (Fragmento React) */}
+
+      <main>
+        <section>
+          <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
+          <p>Este é um exemplo de aplicação React.</p>
+
+          <article>
+            <h3>Artigo 1 de exemplo</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Asperiores, et voluptatem dignissimos dolorum necessitatibus sint
+              quod consectetur aliquid! Officiis adipisci totam natus dicta
+              laborum quisquam fugit corrupti nesciunt, quasi sed?
+            </p>
+          </article>
+
+          <article>
+            <h3>Artigo 2 de exemplo</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Asperiores, et voluptatem dignissimos dolorum necessitatibus sint
+              quod consectetur aliquid! Officiis adipisci totam natus dicta
+              laborum quisquam fugit corrupti nesciunt, quasi sed?
+            </p>
+          </article>
+
+          <article>
+            <h3>Artigo 3 de exemplo</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Asperiores, et voluptatem dignissimos dolorum necessitatibus sint
+              quod consectetur aliquid! Officiis adipisci totam natus dicta
+              laborum quisquam fugit corrupti nesciunt, quasi sed?
+            </p>
+          </article>
+        </section>
+      </main>
+
+      <footer>
+        <h2>Pequeno site criado com React</h2>
+        <p>
+          Desenvolvido por <b>Tiago</b> &copy; 2025
+        </p>
+      </footer>
     </>
   );
 }
