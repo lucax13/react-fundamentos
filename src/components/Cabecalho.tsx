b4ad61c39c5853b5f20713a933f90be0782f31b5
lucax13 <lucas13lima2006@gmail.com>
2025-06-24 11:00:35 -0300

    .

--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -1,11 +1,10 @@
-/* Tudo o que for usado dentro de componentes (imagens, fontes etc), 
-deve ser importado. Desta forma, no momento do build, haverá otimizações para este conteúdo. */
 import logo from "../assets/logo-react.png";
 import Menu from "./Menu";
+import estilos from "./Cabecalho.module.css";
 
 export default function Cabecalho() {
   return (
-    <header>
+    <header className={estilos.topo}>
       <h1>
         <img src={logo} alt="" /> Olá React! 😎
       </h1>
