144fcef43b0b504e435e2048aa2d5e1cb9878f7d
lucax13 <lucas13lima2006@gmail.com>
2025-06-24 09:32:25 -0300

    css

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,7 @@
-// Ajuda detectar uso de recursos obsoletos (APIS, funções, padrões etc)
 import { StrictMode } from "react";
-
-// Usado para criar a 'raíz' da aplicação React a partir de um elemento HTML
 import { createRoot } from "react-dom/client";
-
-// Importa o componente principal da aplicação.
-// O componente App é o ponto de entrada da aplicação React
 import App from "./App.tsx";
-
-// Seleciona o elemento HTML root, cria a raíz da aplicação e renderiza o componente App.
+import "./global.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
