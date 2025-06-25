33e1aede056aa00300c59abc2a613da8d00f51ef
lucax13 <lucas13lima2006@gmail.com>
2025-06-25 08:47:49 -0300

    module menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,19 +2,10 @@ import estilos from "./Menu.module.css";
 
 export default function Menu() {
   return (
-    <nav className={estilos.menus}>
-      <a className={estilos.menu} href="">
-        {" "}
-        Home
-      </a>
-      <a className={estilos.menu} href="">
-        {" "}
-        Sobre
-      </a>
-      <a className={estilos.menu} href="">
-        {" "}
-        Contato
-      </a>
+    <nav className={estilos.menu}>
+      <a href="">Home</a>
+      <a href="">Sobre</a>
+      <a href="">Contato</a>
     </nav>
   );
 }
