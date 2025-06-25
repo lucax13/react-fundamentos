244ea81d6f4096cb17552dba0878fdcbe2492921
lucax13 <lucas13lima2006@gmail.com>
2025-06-25 11:47:50 -0300

    Update AvisoImportante.tsx

--- src/components/Conteudo/AvisoImportante/AvisoImportante.tsx
+++ src/components/Conteudo/AvisoImportante/AvisoImportante.tsx
@@ -1,3 +1,17 @@
 export default function aviso() {
-  return <div>AvisoImportante</div>;
+  return (
+    <section
+      className="bg-yellow-200 border-1-4 border-yellow-600 p-4 mt-4
+    rounded"
+    >
+      <h3 className="font-bold text-2xl text-orange-800">Aviso importante:</h3>
+      <p>Alguns alunos e alunas estão perigando reprovação nesta UC.</p>
+
+      <div className="flex justify-around my-3 bg-amber-300">
+        <div className="bg-amber-900">UM</div>
+        <div className="bg-amber-700">DOIS</div>
+        <div className="bg-amber-500">TRES</div>
+      </div>
+    </section>
+  );
 }
