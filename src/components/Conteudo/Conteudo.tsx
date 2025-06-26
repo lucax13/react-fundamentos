4c6efd4cd9585ceb889cb7a3fddb599c721d46a3
lucax13 <lucas13lima2006@gmail.com>
2025-06-26 08:58:05 -0300

    props

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -8,8 +8,10 @@ export default function Conteudo() {
   return (
     <main className={estilos.conteudo}>
       <section className={`${estilos.arredondada} ${estilos.sombra}`}>
-        <Saudacao nome="Lucas" />
+        <h2 className="font-bold text-lg">Fundamentos de React!</h2>
 
+        <Saudacao nome="Lucas" classe="bg-emerald-400" />
+        <Saudacao nome="Noia" classe="bg-red-100" />
         <p>Este é um exemplo de aplicação React.</p>
 
         <Artigo />
--- src/components/Conteudo/Saudacao.tsx
+++ src/components/Conteudo/Saudacao.tsx
@@ -1,14 +1,13 @@
 /*Deeclara um tipo que séra usado pelas props de componente */
 type Props = {
   nome: string;
+  classe: string;
 };
 
 export default function Saudacao(props: Props) {
   return (
     <>
-      <h2 className="font-bold text-lg">Fundamentos de React!</h2>
-
-      <p className="my-2">
+      <p className={`my-2 rounded text-center ${props.classe}`}>
         Bem-vindo(a) <b>{props.nome}</b>
       </p>
     </>
